961a6935cb5e3c8c8fd66538d48d3864d20c4a3e
wanuchar <61377352+wanuchar@users.noreply.github.com>
2020-04-25 11:50:16 +0300

    Add files via upload

---
+++ laba3/mylist.h
@@ -0,0 +1,34 @@
+#ifndef MYLIST_H
+#define MYLIST_H
+
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+class Enrolle
+{
+    public:
+    string name;
+    vector<int> marks;
+    Enrolle* next;
+    Enrolle(string sname, vector<int> vmarks);
+};
+
+class MyList
+{
+
+public:
+    Enrolle* head;
+    Enrolle* tail;
+
+    MyList();
+    ~MyList();
+    void add(Enrolle* item);
+    void add(Enrolle* item, int ind);
+    void remove(int ind);
+    Enrolle* search(string nameSearch);
+};
+
+#endif // MYLIST_H
